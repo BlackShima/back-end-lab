0804221423c677156613c0a187a8e3e69ae93281
BlackShima <blackshima02@gmail.com>
2026-09-07 20:59:13 +0700

    5.1 no checkpoint

--- src/main/java/se331/lab/service/EventServiceImpl.java
+++ src/main/java/se331/lab/service/EventServiceImpl.java
@@ -24,8 +24,10 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
-
     public Event getEvent(Long id) {
         return eventDao.getEvent(id);
     }
+
+    @Override
+    public Event save(Event event) { return eventDao.save(event); }
 }
